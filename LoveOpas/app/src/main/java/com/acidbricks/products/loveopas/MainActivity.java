e524c234c75fc993d18f71c5db6eaac6d0e2b73f
Talha Hasan Zia <talhahasanzia@hotmail.com>
2016-03-06 17:31:21 +0500

    Background
    Added background

--- LoveOpas/app/src/main/java/com/acidbricks/products/loveopas/MainActivity.java
+++ LoveOpas/app/src/main/java/com/acidbricks/products/loveopas/MainActivity.java
@@ -1,13 +1,29 @@
 package com.acidbricks.products.loveopas;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+       // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
